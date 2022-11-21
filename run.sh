c40ddafa690d731e8586a319743fd0798bfc92d3
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2022-11-21 14:20:53 +0100

    comments etc

--- run.sh
+++ run.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
+### -- set the job Name -- 
 #BSUB -J TCAV
+### -- logging files -- 
 #BSUB -o tcav_%J.out
 #BSUB -e tcav_%J.err
+### -- specify queue -- 
 #BSUB -q hpc
+### -- ask for 1 core -- 
+#BSUB -n 10 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 2GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=2GB]"
+### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
+#BSUB -M 3GB
+### -- set walltime limit: hh:mm --
 #BSUB -W 12:00
-#BSUB -N 2
-#BSUB -R “rusage[mem=2GB]”
+### -- send notification to email --
 #BSUB -u s204163@dtu.dk
+### -- send notification at start -- 
 #BSUB -B
+### -- send notification at completion -- 
 #BSUB -N
-#BSUB -Ne
 python3 download_and_make_datasets.py --number_of_images_per_folder=2 --number_of_random_folders=2
-python3 tcav_hpc.py --num_exp=2 --save_file_name=results_20221121
+python3 tcav_hpc.py --num_exp=2 --save_filename=results_20221121
