d1ca5d995ee9ecc86cdea417c63692652dee84ff
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2022-11-29 20:01:11 +0100

    should work now

--- download_and_make_datasets.py
+++ download_and_make_datasets.py
@@ -1,62 +1,35 @@
-"""
-Copyright 2018 Google LLC
+# run on mac
+# https://developer.apple.com/metal/tensorflow-plugin/
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-""" Downloads models and datasets for imagenet
-
-    Content downloaded:
-        - Imagenet images for the zebra class.
-        - Full Broden dataset(http://netdissect.csail.mit.edu/)
-        - Inception 5h model(https://github.com/Hvass-Labs/TensorFlow-Tutorials/blob/master/inception5h.py)
-        - Mobilenet V2 model(https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet_v1.md)
-
-    Functionality:
-        - Downloads open source models(Inception and Mobilenet)
-        - Downloads the zebra class from imagenet, to illustrate a target class
-        - Extracts three concepts from the Broden dataset(striped, dotted, zigzagged)
-        - Structures the data in a format that can be readily used by TCAV
-        - Creates random folders with examples from Imagenet. Those are used by TCAV.
-
-    Example usage:
-
-    python download_and_make_datasets.py --source_dir=YOUR_FOLDER --number_of_images_per_folder=50 --number_of_random_folders=10
-"""
 import subprocess
 import os
 import argparse
 from tensorflow.io import gfile
 import imagenet_and_broden_fetcher as fetcher
 
-def make_concepts_targets_and_randoms(source_dir:str, number_of_images_per_folder:int, number_of_random_folders:int, imagenet_classes:list, broden_concepts:list):
+def make_concepts_targets_and_randoms(source_dir: str, number_of_images_per_folder: int, number_of_random_folders: int, imagenet_classes: list, broden_concepts: list):
     # Run script to download data to source_dir
-    if not gfile.exists(source_dir):
-        gfile.makedirs(source_dir)
-    if not gfile.exists(os.path.join(source_dir,'broden1_224/')) or not gfile.exists(os.path.join(source_dir,'inception5h')):
-        subprocess.call(['bash' , 'FetchDataAndModels.sh', source_dir])
+    if not gfile.exists(os.path.join(source_dir, 'broden1_224/')) or not gfile.exists(os.path.join(source_dir, 'inception5h')):
+        print("Running FetchDataAndModels.sh to get broden1_224, inception5h and mobilenet")
+        subprocess.call(['bash', 'FetchDataAndModels.sh', source_dir])
 
     # make targets from imagenet
     imagenet_dataframe = fetcher.make_imagenet_dataframe("./imagenet_url_map.csv")
     for image in imagenet_classes:
+        print(f"Downloading {number_of_images_per_folder} images to data/{image}")
         fetcher.fetch_imagenet_class(source_dir, image, number_of_images_per_folder, imagenet_dataframe)
 
     # Make concepts from broden
     for concept in broden_concepts:
-        fetcher.download_texture_to_working_folder(broden_path=os.path.join(source_dir, 'broden1_224'),
-                                                   saving_path=source_dir,
-                                                   texture_name=concept,
-                                                   number_of_images=number_of_images_per_folder)
+        print(f"Downloading images for concept: {concept}")
+        fetcher.download_texture_to_working_folder(
+            broden_path=os.path.join(source_dir, 'broden1_224'),
+            saving_path=source_dir,
+            texture_name=concept,
+            number_of_images=number_of_images_per_folder
+        )
 
+    print("Starting function call fetcher.generate_random_folders")
     # Make random folders. If we want to run N random experiments with tcav, we need N+1 folders.
     fetcher.generate_random_folders(
         working_directory=source_dir,
@@ -67,26 +40,24 @@ def make_concepts_targets_and_randoms(source_dir:str, number_of_images_per_folde
     )
 
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create examples and concepts folders.')
-    # parser.add_argument('--source_dir', type=str,
-    #                     help='Name for the directory where we will create the data.')
-    parser.add_argument('--number_of_images_per_folder', type=int, help='Number of images to be included in each folder')
-    parser.add_argument('--number_of_random_folders', type=int, help='Number of folders with random examples that we will generate for tcav')
-    parser.add_argument("--data_path", type=str, nargs="?", default="data", help="The name of the folder where the data is stored, defaults to data")
+    parser = argparse.ArgumentParser(
+        description='Create examples and concepts folders.')
+    
+    parser.add_argument('--num_images', type=int, help='Number of images to be included in each folder')
+    parser.add_argument('--num_folders', type=int, help='Number of folders with random examples that we will generate for tcav')
     parser.add_argument("--targets", type=str, nargs="+", default=["zebra"], help="The name of imagenet classes to use, defaults to zebra")
     parser.add_argument("--concepts", type=str, nargs="+", default=['striped', 'dotted', 'zigzagged'], help="The broden concepts to use, defaults to 'striped, dotted, zigzagged'")
 
     args = parser.parse_args()
-
-    source_dir = "tcav/" + args.data_path
+    
+    source_dir = "tcav/data"
 
     # create folder if it doesn't exist
     if not gfile.exists(source_dir):
         gfile.makedirs(os.path.join(source_dir))
         print("Created source directory at " + source_dir)
+    
     # Make data
-    make_concepts_targets_and_randoms(source_dir, args.number_of_images_per_folder, args.number_of_random_folders, args.targets, args.concepts)
+    make_concepts_targets_and_randoms(source_dir, args.num_images, args.num_folders, args.targets, args.concepts)
     print("Successfully created data at " + source_dir)
-
--- tcav_hpc.py
+++ tcav_hpc.py
@@ -13,10 +13,8 @@ if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description='Create examples and concepts folders.')
     parser.add_argument("--num_exp", type=int, help="The number of random experiments, the same as the number of random folders")
-    parser.add_argument("--save_filename", type=str, help="The name to save the results to. DONT ADD .JSON!")
+    parser.add_argument("--save_name", type=str, help="The name to save the results to. DONT ADD .JSON!")
     
-    # parser.add_argument("--working_dir", type=str, nargs="?", default="tcav", help="The path to the /tcav folder. i.e. /zhome/c9/c/156514/Desktop/project-tcav-master/tcav")
-    parser.add_argument("--data_path", type=str, nargs="?", default="data", help="The name of the folder where the data is stored, defaults to data")
     parser.add_argument("--model_to_run", type=str, nargs="?", default="GoogleNet", help="The model to run, defaults to GoogleNet")
     parser.add_argument("--target", type=str, nargs="?", default="zebra", help="The taget of the model, defaults to zebra")
     parser.add_argument("--bottlenecks", type=str, nargs="+", default=['mixed3a','mixed3b','mixed4a','mixed4b','mixed4c','mixed4d','mixed4e','mixed5a','mixed5b'], help="The layers to look at")
@@ -25,22 +23,21 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     model_to_run = args.model_to_run
-    working_dir = "tcav" #args.working_dir
     
     # where activations are stored (only if your act_gen_wrapper does so)
+    working_dir = "tcav"
     activation_dir =  working_dir + '/activations/'
     cav_dir = working_dir + '/cavs/'
-    source_dir = working_dir + f'/{args.data_path}/'
+    source_dir = working_dir + '/data/'
 
-    utils.make_dir_if_not_exists(activation_dir)
     utils.make_dir_if_not_exists(working_dir)
+    utils.make_dir_if_not_exists(activation_dir)
     utils.make_dir_if_not_exists(cav_dir)
 
     sess = utils.create_session()
-
     
-    GRAPH_PATH = f'{working_dir}/{args.data_path}/inception5h/tensorflow_inception_graph.pb'
-    LABEL_PATH = f'{working_dir}/{args.data_path}/inception5h/imagenet_comp_graph_label_strings.txt'
+    GRAPH_PATH = 'tcav/data/inception5h/tensorflow_inception_graph.pb'
+    LABEL_PATH = 'tcav/data/inception5h/imagenet_comp_graph_label_strings.txt'
 
     mymodel = model.GoogleNetWrapper_public(sess, GRAPH_PATH, LABEL_PATH)
     act_generator = act_gen.ImageActivationGenerator(mymodel, source_dir, activation_dir, max_examples=100)
@@ -54,11 +51,12 @@ if __name__ == '__main__':
         alphas = [0.1],
         cav_dir = cav_dir,
         num_random_exp = args.num_exp
+        #random_concepts=? se original Run_TCAV.ipynb
     )
     
     print('This may take a while... Go get corny!')
     results = mytcav.run(run_parallel=False)
-    print('done!')
+    print('done! - check /results/')
 
-    with open(f"results/{args.save_filename}.json", "w") as outfile:
+    with open(f"results/{args.save_name}.json", "w") as outfile:
         json.dump(results, outfile)
